ef72c968f8cfd5ee5bbdb22b685cb028c1462692
sodino <sodino@qq.com>
2013-05-30 15:12:59 +0800

    data type:environment完成

--- ant_demo/build_06_02_arg_env.bat
+++ ant_demo/build_06_02_arg_env.bat
@@ -1,1 +1,8 @@
-echo ${env.jad_home}
+@echo off 
+echo REM DOS系统引用环境变量的方式: %%env.key%%
+echo Unix系统引用环境变量的方式: $env.key
+echo 使用Jad工具反编译.class文件并将反编译结果输出到output_dir中
+
+del %output_dir%\AntTest.jad
+
+%jad_home%\jad  -d %output_dir% %class_file%
